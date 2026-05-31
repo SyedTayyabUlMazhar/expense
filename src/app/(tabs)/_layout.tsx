dd55e9737bb87ecb2266cbd123ea1007f9389278
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2026-05-31 22:12:30 +0500

    refactor: move app source files to src directory and update tsconfig paths

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -40,7 +40,7 @@ export default function TabLayout() {
                   <SymbolView
                     name={{ ios: "info.circle", android: "info", web: "info" }}
                     size={25}
-                    tintColor={Colors[colorScheme].text}
+                    tintColor={Colors[colorScheme].foreground}
                     style={{ opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -54,7 +54,7 @@ export default function TabLayout() {
                   <SymbolView
                     name={{ ios: "info.circle", android: "info", web: "info" }}
                     size={25}
-                    tintColor={Colors[colorScheme].text}
+                    tintColor={Colors[colorScheme].foreground}
                     style={{ opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
